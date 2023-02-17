4db0ca36a27c07427a229ca19658d81c32086b51
Ryohei Udagawa <r.udagawa1995@gmail.com>
2023-02-17 18:46:44 +0900

    :+1: 接続先をread_replicaにすることで、primaryに負荷がかからないように変更

--- lib/pgdump_scrambler/dumper.rb
+++ lib/pgdump_scrambler/dumper.rb
@@ -49,7 +49,7 @@ module PgdumpScrambler
         db_config = open(Rails.root.join('config', 'database.yml'), 'r') do |f|
           YAML.load(f)
         end
-        db_config[Rails.env]
+        db_config[Rails.env].key?('read_replica') ? db_config[Rails.env]['read_replica'] : db_config[Rails.env]
       end
     end
   end
